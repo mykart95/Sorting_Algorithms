a5e3760ba7e49c9ccc9589d7efbbc836f13c5fd1
mykart95 <karthikmy786@gmail.com>
2025-01-29 11:04:48 +0530

    working bogo

---
+++ src/com/sorting/bogo/App.java
@@ -0,0 +1,11 @@
+package com.sorting.bogo;
+
+public class App {
+
+	public static void main(String[] args) {
+		int[] nums= {5,2,8,10,1,0,11};
+		BogoSort sor=new BogoSort(nums);
+		sor.sort();
+	}
+
+}
--- src/com/sorting/bogo/BogoSort.java
+++ src/com/sorting/bogo/BogoSort.java
@@ -2,4 +2,49 @@ package com.sorting.bogo;
 
 public class BogoSort {
 
+	private int[] nums;
+	public int counter;
+	
+	public BogoSort(int num[]) {
+		this.nums=num;
+	}
+	
+	public void sort() {
+		
+		while(!isSorted()) {
+			counter++;
+			shuffle();
+		}
+		showSortedArray();
+
+	}
+
+	//fisher and yates
+	private void shuffle() {
+		for (int i = nums.length-1; i >=0 ; i--) {
+			int j=(int) (Math.random()*i);
+			swap(i, j);
+		}
+	}
+
+	private void showSortedArray() {
+		System.out.println("No of Iteration = " +counter);
+		for (int i = 0; i < nums.length; i++) {
+			System.out.print(nums[i]+" ");
+		}
+	}
+
+	private void swap(int i, int j) {
+		int temp=nums[i];
+		nums[i]=nums[j];
+		nums[j]=temp;
+	}
+
+	private boolean isSorted() {
+		for(int i=0; i<nums.length-1; i++) {
+			if(nums[i+1]<nums[i])
+				return false;
+		}
+ 		return true;
+	}
 }
