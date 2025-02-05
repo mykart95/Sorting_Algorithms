37464e8ebe77bff48ffaa2cbfd3b7c2cdcc0461f
mykart95 <karthikmy786@gmail.com>
2025-02-05 09:27:43 +0530

    ShellSort Incomplete

---
+++ src/com/sorting/shell/ShellSort.java
@@ -0,0 +1,35 @@
+package com.sorting.shell;
+
+public class ShellSort {
+
+	private int[] nums;
+
+	public ShellSort(int nums[]) {
+		this.nums=nums;
+	}
+	
+	public void sort() {
+		for (int gap = nums.length/2; gap>0; gap/=2) {
+			
+			for (int i = gap; i < nums.length; i++) {
+				int j=i;
+					
+				while(j>gap && nums[j-gap]>nums[j]) {
+					swap(j, j-gap);
+					j-=gap;
+				}
+				}
+				
+			}
+		for(int i=0; i<nums.length; i++) {
+			System.out.print(nums[i]+" ");
+		}
+		}
+
+	private void swap(int i, int j) {
+		int temp=nums[i];
+		nums[i]=nums[j];
+		nums[j]=temp;
+	}
+}
+
